4277d08bebdde217555049d41e0a1302e3273118
Sukanya <suakanyaa26@gmail.com>
2023-04-30 11:04:39 +0700

    create vat project
    coding

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log();
+function sevenPercent(price) {
+    let vat = price * 7 / 100
+    let value = parseInt(price) + vat
+    return value.toFixed(2) + " baht"
+}
+function threePercent(price) {
+    let vat = price * 3 / 100
+    let value = parseInt(sevenPercent(price)) - vat
+    return value.toFixed(2) + " baht"
+}
+function fivePercent(price) {
+    let vat = price * 5 / 100
+    let value = parseInt(sevenPercent(price)) - vat
+    return value.toFixed(2) + " baht"
+}
+function sevenPercentProgram(value) {
+    alert(sevenPercent(value))   //Program start run from here 
+}
+function threePercentProgram(value) {
+    alert(threePercent(value))   //Program start run from here 
+}
+function fivePercentProgram(value) {
+    alert(fivePercent(value))   //Program start run from here 
+}
